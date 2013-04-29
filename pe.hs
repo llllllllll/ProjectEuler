f123418c92286cbf0fd110cf78178b820769eeeb
Joe Jevnik <JoeJev@gmail.com>
2013-04-29 02:48:50 -0400

    added output to comments

--- pe.hs
+++ pe.hs
@@ -14,32 +14,32 @@ primes = 2: 3: sieve (tail primes) [5,7..]
 
 -- Problems 
 
-{- Completed 29.4.2013 -}
+{- 233168 - Completed 29.4.2013 -}
 problem_1 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 
-{- Completed 29.4.2013 -}
+{- 4613732 - Completed 29.4.2013 -}
 problem_2 = sum [x | x <- takeWhile (<4000000) (map fib [2..]), even x]
 
 
 problem_3 = error "Not Completed In Haskell"
 
-{- Completed 29.4.2013 -}
+{- 906609 - Completed 29.4.2013 -}
 problem_4 = maximum [x*y | x <- [100..999], y <- [100..999], reverse (show (x*y)) == show (x*y)]
 
-{- Completed 29.4.2013 -}
+{- 232792560 - Completed 29.4.2013 -}
 problem_5 = foldl (lcm) 1 [1..20]
 
-{- Completed 29.4.2013 -}
+{- 25164150 - Completed 29.4.2013 -}
 problem_6 = (sum [1..100]) ^ 2 - sum (map (^2) [1..100])
 
-{- Completed 29.4.2013 -}
+{- 104743 - Completed 29.4.2013 -}
 problem_7 = primes !! 10001
 
 problem_8 = error "Not Completed In Haskell"
 
 problem_9 = error "Not Completed In Haskell"
 
-{- Completed 29.4.2013 -}
+{- 142913828922 - Completed 29.4.2013 -}
 problem_10 = sum $ takeWhile (<2000000) primes
 
 problem_11 = error "Not Completed In Haskell"
