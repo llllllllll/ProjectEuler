cde926ee34fa716505fcccc86d4d116ec3637ef0
Joe Jevnik <JoeJev@gmail.com>
2013-05-05 23:30:17 -0400

    clean problem_52

--- pe.hs
+++ pe.hs
@@ -100,3 +100,7 @@ problem_34 = sum [x | x <- [3..99999], is_curious x]
 
 {- 9110846700 - Completed 5.5.2013 -}
 problem_48 = reverse $ take 10 $ (reverse . show . sum) (map (\x -> x^x) [1..1000])
+
+problem_52 = head [n | n <- [1..], is_valid n]
+	where
+		is_valid n = (sort . int_to_list) n == ((nub . sort . concat . map (\x -> int_to_list (x*n))) [2..6])
