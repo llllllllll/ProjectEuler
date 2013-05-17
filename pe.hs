5f288135bd6da73bdf52c94174dead617179ff44
Joe Jevnik <JoeJev@gmail.com>
2013-05-17 02:59:37 -0400

    problems 3, 23, 47, 71, 72
    Cleaned a lot of comments.

--- pe.hs
+++ pe.hs
@@ -9,22 +9,22 @@ import Data.Ratio
 
 --Lists--
 
-{- An infinite list of prime numbers -}
+{-An infinite list of prime numbers-}
 primes :: [Integer]
 primes = 2: 3: sieve (tail primes) [5,7..]
 	where 
   		sieve (p:ps) xs = h ++ sieve ps [x | x <- t, x `rem` p /= 0]  
   			where (h,~(_:t)) = span (< p*p) xs
 
-{- A list of all divisors of n -}
+{-A list of all divisors of n-}
 divisors :: Integral a => a -> [a]
 divisors n = [x | x <- takeWhile (<=ceiling ((fromIntegral n)/2)) [1..], n `mod` x == 0]
 
-{- A list of all divisors of n in reverse order -}
+{-A list of all divisors of n in reverse order-}
 r_divisors :: Integral a => a -> [a]
 r_divisors n = [x | x <- [n, n-1..1], n `mod` x == 0]
 
-{- The primes factors of n -}
+{-The primes factors of n-}
 prime_factors n = factor primes n
   where 
     factor ps@(p:pt) n | p*p > n      = [n]               
@@ -32,20 +32,21 @@ prime_factors n = factor primes n
                        | otherwise    =     factor pt n
 prime_factors_mult = map encode . group . prime_factors
     where encode xs = (head xs, length xs)
-{- Euler's number -}
+
+{-Euler's number-}
 e = exp 1
 
-{- Champernown's constant -}
+{-Champernown's constant-}
 champernowne = concatMap show [0..]
 
 --Sequences--
 
-{- Fibonacci Sequence starting at n -}
+{-Fibonacci Sequence starting at n-}
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
-{- Collatz Sequence starting at n -}
+{-Collatz Sequence starting at n-}
 collatz :: Integral a => a -> [a]
 collatz 1 = [1]
 collatz n
@@ -54,61 +55,76 @@ collatz n
 
 --Functions--
 
-{- factorial n = n! -}
+{-factorial n = n!-}
 factorial n = product [1..n]
 
-{- The number of divisors of n -}
+{-The number of divisors of n-}
 num_divisors n = (length . divisors) n
 
-{- Bool whether n is a prime number or not -}
+{-Bool whether n is a prime number or not-}
 is_prime n = n == (head $ primes \\ (takeWhile (<n) primes))
 
-{- convert an Integral to a list of its digits. eg: int_to_list 123 = [1,2,3] -}
+{-convert an Integral to a list of its digits. eg: int_to_list 123 = [1,2,3]-}
 int_to_list n = map digitToInt $ show n
 
-{- Inverse of int_to_list. Convert a list into an Integral where each element is a digit. eg: list_to_int [1,2,3] = 123 -}
+{-Inverse of int_to_list. Convert a list into an Integral where each element is a digit. eg: list_to_int [1,2,3] = 123-}
 list_to_int ns = read $ concatMap show ns :: Integer
 
-{- Returns a Bool whether a number is neither increasing or decreasing -}
+{-Returns a Bool whether a number is neither increasing or decreasing-}
 is_bouncy n = not (is_increasing (int_to_list n) || is_decreasing (int_to_list n))
-{- Returns a Bool as to whether a number is increasing. eg: is_increasing 1234 = True, is_increasing 4321 = False -}
+{-Returns a Bool as to whether a number is increasing. eg: is_increasing 1234 = True, is_increasing 4321 = False-}
 is_increasing n = (sort . show) n == (show n)
-{- Returns a Bool as to whether a number is decreasing. eg: is_increasing 1234 = False, is_increasing 4321 = True -}
+{-Returns a Bool as to whether a number is decreasing. eg: is_increasing 1234 = False, is_increasing 4321 = True-}
 is_decreasing n = (reverse . sort . show) n == (show n)
 
-{- Returns if n is pandigital with bounds a..b -}
+{-Returns if n is pandigital with bounds a..b-}
 is_pandigital a b n = (nub . sort . int_to_list) n == [a..b]
 is_pandigital' a b n = (sort . int_to_list) n == [a..b]
 
-{- Returns the first d digits of n -}
+{-Returns the first d digits of n-}
 head_n d n = list_to_int $ take d (int_to_list n)
-{- Returns the last d digits of n -}
+{-Returns the last d digits of n-}
 last_n d n = reverse $ take d $ (reverse . show) n
 
-{- Number of combinations of n choose r -}
+{-Number of combinations of n choose r-}
 n `nCr` r = factorial n / (factorial r * factorial (n-r))
 
-{- returns the number of times n occurs in list ns -}
+{-returns the number of times n occurs in list ns-}
 elem_count n ns =  (length . nubBy (/=)) (n:ns) - 1
 
-{- Returns if n is an Integral to 7 decimal places -}
+{-Returns if n is an Integral to 7 decimal places-}
 is_int :: RealFrac a => a -> Bool
 is_int n = (round $ 10^(fromIntegral 7)*(n-(fromIntegral $ round n))) == 0
 
-{- Returns True if n is a perfect square -}
+{-Returns True if n is a perfect square-}
 is_square n = is_int (fromIntegral (n) **(0.5))
 
-{- The size of the list of numbers coprime to n -}
+{-The size of the list of numbers coprime to n-}
 euler_totient m = product [(p - 1) * p ^ (c - 1) | (p, c) <- prime_factors_mult m]
 
-{- Detirmines if a number is a lychrel number, a number that does not converge to a palindrome -}
+{-Farey Sequence-}
+farey_seq a b
+    |da2<=10^6=farey_seq a1 b
+    |otherwise=na
+    where
+    	na=numerator a
+    	nb=numerator b
+    	da=denominator a
+    	db=denominator b
+    	a1=(na+nb)%(da+db)
+    	da2=denominator a1
+
+{-The length of the farey Sequence of order n.-}
+farey_seq_length n = (sum . map euler_totient) [1..n]
+
+{-Detirmines if a number is a lychrel number, a number that does not converge to a palindrome-}
 is_lychrel n = is_lychrel' n 0
 is_lychrel' n c
 	| (reverse . show) n == show n && c /= 0 = False
 	| c == 50 = True
 	| otherwise = is_lychrel' (list_to_int ((reverse . int_to_list) n) + n) (c+1)
 
-{- A Binary Search-}
+{-A Binary Search for arays-}
 binary_search n ns = binary_search' n ns 0 ((snd . bounds) ns - (fst . bounds) ns)
 binary_search' n ns a b
 	| ns ! c == n = c
@@ -120,36 +136,36 @@ binary_search' n ns a b
 
 --Problems--
 
-{- 233168 - Completed 29.4.2013 -}
+{-233168 - Completed 29.4.2013-}
 problem_1 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 problem_1' = (sum . nub) $ union [3,6..999] [5,10..995]
 
-{- 4613732 - Completed 29.4.2013 -}
+{-4613732 - Completed 29.4.2013 - Learned list comprehensions-}
 problem_2 = sum [x | x <- takeWhile (<4000000) (map fib [2..]), even x]
 
+{-6857 - Completed 17.5.2013 - Forgot to fix after new prime_factors was written.-}
+problem_3 = last $ prime_factors 600851475143
 
-problem_3 = error "Not Completed In Haskell"
-
-{- 906609 - Completed 29.4.2013 -}
+{-906609 - Completed 29.4.2013 - Learned how to use multiple generators.-}
 problem_4 = maximum [x*y | x <- [100..999], y <- [100..999], reverse (show (x*y)) == show (x*y)]
 
-{- 232792560 - Completed 29.4.2013 -}
+{-232792560 - Completed 29.4.2013 - Learned about folds-}
 problem_5 = foldl (lcm) 1 [1..20]
 
-{- 25164150 - Completed 29.4.2013 -}
+{-25164150 - Completed 29.4.2013 - Learned about map-}
 problem_6 = (sum [1..100]) ^ 2 - sum (map (^2) [1..100])
 
-{- 104743 - Completed 29.4.2013 -}
+{-104743 - Completed 29.4.2013 - Learned how to index a list-}
 problem_7 = primes !! 10001
 
 problem_8 = error "Not Completed In Haskell"
 
 problem_9 = error "Not Completed In Haskell"
 
-{- 142913828922 - Completed 29.4.2013 -}
+{-142913828922 - Completed 29.4.2013-}
 problem_10 = sum $ takeWhile (<2000000) primes
 
-{- 70600674 - Completed 16.5.2013 -}
+{-70600674 - Completed 16.5.2013 - Learned arrays and reasoned through 2d arrays-}
 problem_11 = maximum [maximum [horizontal r n | r <- [0..19], n <- [0..15]], maximum [vertical r n | r <- [0..15], n <- [0..19]], maximum [d_right r n | r <- [0..15], n <- [0..15]], maximum [d_left r n | r <- [3..15], n <- [3..15]]]
 	where 
 		horizontal r n = grid!r!n * grid!r!(n+1) * grid!r!(n+2) * grid!r!(n+3)
@@ -182,27 +198,27 @@ problem_12 = head [tri_num x | x <- [1..], (num_divisors . tri_num) x > 500]
 	where
 		tri_num t = sum [1..t]
 
-{- 5537376230 - Completed 15.5.2013 -}
+{-5537376230 - Completed 15.5.2013-}
 problem_13 = head_n 10 a
 	where a = 0 -- Sum all the numbers on the site.
 
-{- 837799 - Completed 29.4.2013 -}
+{-837799 - Completed 29.4.2013-}
 problem_14 =  head (head [collatz x | x <- [999999,999998..1], (length (collatz x))  == maximum [length (collatz x) | x <- [1..999999]]])
 
-{- 137846528820 - Completed 17.5.2013 -}
-problem_15 = 40 `nCr` 20 --Binomial Coefficient for solving lattice paths.
+{-137846528820 - Completed 17.5.2013 - Learned about binomial coefficients for solving lattice paths.-}
+problem_15 = 40 `nCr` 20 
 
-{- 648 - Completed 11.5.2013 -}
+{-648 - Completed 11.5.2013-}
 problem_20 = (sum . int_to_list) $ factorial 100
 
-{- 31725 - Completed 12.5.2013 -}
+{-31725 - Completed 12.5.2013 - Started working on typing-}
 problem_21 = sum [a | a <- [1..9999], is_amicable a]
 	where
 		is_amicable :: Integral a => a -> Bool
 		is_amicable a = (sum . divisors) ((sum . divisors) a) == a && a /= (sum . divisors) a
 
 
-{- 871198282 - Completed 5.5.2013 -}
+{-871198282 - Completed 5.5.2013 - Baby's first lambda-}
 problem_22 = sum $ map (\n -> raw_score n * pos_mod n) names
 	where
 		names = [""] --Names.txt can be found on the website, I just ommited to clean it up.
@@ -210,22 +226,27 @@ problem_22 = sum $ map (\n -> raw_score n * pos_mod n) names
 		char_pos c = (fromEnum c) - (fromEnum 'A') + 1
 		pos_mod n = length (takeWhile (/=n) names) + 1
 
-{- 2783915460 - Completed 5.5.2013 -}
+problem_23 = sum [n | n <- [1..28123], is_valid n]
+	where
+		is_valid n = null [0 | a <- [1..n], b <- [n-1,n-2..1], (a+b) == n && is_abundant a && is_abundant b]
+		is_abundant n = (sum . divisors) n > n
+
+{-2783915460 - Completed 5.5.2013-}
 problem_24 =  (sort . permutations) ['0'..'9'] !! 999999
 
 problem_25 = head [x | x <- [1..], (length . show . fib) x == 1000]
 
-{- 9183 - Completed 3.5.2013 -}
+{-9183 - Completed 3.5.2013-}
 problem_29 = length $ nub [a^b | a <- [2..100], b <- [2..100]] 
 
-{- 443839 - Completed 5.5.2013 -}
+{-443839 - Completed 5.5.2013-}
 problem_30 = sum [s | s <- [2..999999], ((sum . map (^5)) (int_to_list s)) == s]
 
 problem_32 = sum $ nub [a*b | a <- [1..10000], b <- [1..a], is_valid a b]
 	where
 		is_valid a b = is_pandigital' 1 9 (list_to_int [a*b,a,b])
 
-{- 40730 - Completed 5.5.2013 -}
+{-40730 - Completed 5.5.2013-}
 problem_34 = sum [x | x <- [3..99999], is_curious x]
 	where
 		is_curious n = (sum . map factorial) (int_to_list n) == n
@@ -235,36 +256,56 @@ problem_35 = [1 | n <- takeWhile (<1000000) primes, is_valid n]
 		is_valid n = foldl (&&) (True) $ map (is_prime . list_to_int) ((circulate . int_to_list) n)
 		circulate ns = init (zipWith (++) (tails ns) (inits ns))
 
-{- 210 - Completed 15.5.2013 -}
+{-210 - Completed 15.5.2013 - Leaned about Champernowne's Constant.-}
 problem_40 = product $ map (digitToInt . (!!) champernowne) indecies
 	where
 		indecies = map (10^) [1..6]
 
-{- 9110846700 - Completed 5.5.2013 -}
+{-134043 - Completed 17.5.2013-}
+problem_47 = head [n | n <- [1..], is_valid n]
+	where
+		is_valid n = all (==4) $ map (length . nub . prime_factors) [n..n+3]
+
+{-9110846700 - Completed 5.5.2013 - Wrote my last_n for this.-}
 problem_48 = last_n 10 (sum (map (\x -> x^x) [1..1000]))
 
-{- 142857 - Completed 5.5.2013 -}
+{-142857 - Completed 5.5.2013-}
 problem_52 = head [n | n <- [1..], is_valid n]
 	where
 		is_valid n = (sort . int_to_list) n == ((nub . sort . concat . map (\x -> int_to_list (x*n))) [2..6])
 
-{- 4075 - Completed 8.5.2013 -}
+{-4075 - Completed 8.5.2013 - Wrote nCr and learned about defining functions with infix.-}
 problem_53 = genericLength [1 | n <- [1..100], r <- [1..n], n `nCr` r > 1000000]
 
-{- 249 - Completed 15.5.2013 -}
+{-249 - Completed 15.5.2013 -Learned about Lychrel Numbers.-}
 problem_55 = length [n | n <- [1..9999], is_lychrel n]
 
-{- 972 - Completed 10.5.2013 -}
+{-972 - Completed 10.5.2013-}
 problem_56 = maximum $ [(sum . int_to_list) (a^b) | a <- [1..99], b <- [1..99]]
 
-{- 510510 - Completed 16.5.2013 -}
+{-510510 - Completed 16.5.2013 - Learned Euler Totient (phi(n)).-}
 problem_69 =  last $ sortBy (compare `on` snd) [(n, (fromIntegral n) / (fromIntegral . euler_totient) n) | n <- [2..1000000]]
 
 problem_70 = reverse $ sortBy (compare `on` snd) [(n, (fromIntegral n) / (fromIntegral . euler_totient) n) | n <- [2..10^7]]
 	where
 		is_valid n = (sort . show . euler_totient) n == (sort . show) n
 
-{- 8739992577 - Completed 8.5.2013 -}
+{-428572 - Completed 17.5.2013 - Used Farey Sequences learned in problem 72.-}
+problem_71 = farey_seq (0%1) (3%7)
+
+{-303963552391 - Completed 17.5.2013 - Learned Farey Sequences-}
+problem_72 = farey_seq_length 1000000 
+
+{-402 - Completed 17.5.2013 - Learned about Factorial Chains.-}
+problem_74 = length [f_chain n| n <- [1..999999], is_valid n]
+	where
+		is_valid n = f_chain n == 60
+		f_chain n = f_chain' n []
+		f_chain' n ns
+			| n `elem` ns = length ns
+			| otherwise = f_chain' ((sum . map factorial) (int_to_list n)) (n:ns)
+
+{-8739992577 - Completed 8.5.2013-}
 problem_97 = last_n 10 $ 28433*2^(7830457)+1
 
 problem_104 = head $ filter (\x -> is_pandigital 1 9 (head_n 9 x) && is_pandigital 1 9 (last_n 9 x)) (map (fib) [1..])
@@ -282,7 +323,7 @@ problem_211 = sum [n | n <- [1..64000000], is_valid n]
 		is_valid :: Integral a => a -> Bool
 		is_valid n = is_square $ sum (map (^2) (divisors n))
 
-{- 1.002322108633 (Paper/Pencil)- Completed 11.5.2013 -}
+{-1.002322108633 (Paper/Pencil)- Completed 11.5.2013 - Learned how to binary search by hand.-}
 problem_235 = error "Not Completed In Haskell"
 
 problem_371 =  [(n-1)*p | n <- [1..10]]
